0a25548073a658665801398ba98cc52595b4ad56
Catalina Williams <webdevcatalina@gmail.com>
2021-05-27 14:41:35 -0600

    added code to app.js that posts new data to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+import axios from "axios";
 
 function App() {
 
-  //CONNECTING TO BACKEND DATA AND DISPLAYING THAT DATA ON THE FRONT END
+  
 
-  //1. using state which at first is empty but soon will store data collected from our MongoDB 
+  //using state which at first is empty but soon will store data collected from our MongoDB 
 
     //we are using an array of objects because state is saving multiple posts
 
@@ -17,6 +18,18 @@ function App() {
       date: null,
     }
   ]);
+
+   //setting state variable to hold user entered data
+  //note the single object 
+  const [post, setPost] = useState(
+    {
+      title: " ",
+      author: " ",
+      body: " ",
+      date: null,
+    }
+  );
+//CONNECTING TO BACKEND DATA AND DISPLAYING THAT DATA ON THE FRONT END
 
   //fetch the data from /posts API and return a json
 
@@ -30,13 +43,51 @@ function App() {
       //set state equal to the json response
     }).then(jsonRes => setPosts(jsonRes))
 
-  })
+  });
 
-    
-
+    //POSTING NEW DATA TO THE BACKEND
+  
+   //handleChange tracks what is being typed 
+   // 'e' has two peices of data, the name and value of inputs
+  function handleChange(e) {
+    const {name, value} = e.target;
+    setPost(prevInput => {
+      return(
+        {
+          ...prevInput,
+          [name]: value
+        }
+      )
+    })
+  }
+   //addPost function
+  function addPost(e) {
+    e.preventDefault();
+    //gets value from state variable post
+    const newPost = {
+      title: post.title,
+      author: post.author,
+      body: post.body,
+      date: post.date
+    }
+    //posts newPost variable to proxy 
+    axios.post('/newPost', newPost)
+  }
+  // input names are required to parse from body req in our server
+  //value is set to whatever value was saved in state variable post
   return (
     <div className="App">
-      
+
+      <h1>Add Post</h1>
+
+      <form>
+
+        <input onChange={handleChange} name="title" value={post.title}></input>
+        <input onChange={handleChange} name="author" value={post.author}></input>
+        <input onChange={handleChange} name="body" value={post.body}></input>
+        <input onChange={handleChange} name="date" value={post.date}></input>
+        <button onClick={addPost}>ADD POST</button>
+      </form>
       {posts.map(post => {
 
         return (
