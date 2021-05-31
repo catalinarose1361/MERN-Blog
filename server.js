0c3c00ac0105d17b0b29f778d1b3e542d674121f
Catalina Williams <webdevcatalina@gmail.com>
2021-05-31 13:39:02 -0600

    removed post controller

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-// const routes = require("./routes");
+const routes = require("./routes/api.js")
 const port = process.env.PORT || 5000;
 // const Post = require("./models")
-
+// app.use(require("./routes/api.js"));
 
 require('dotenv').config()
 
@@ -18,7 +18,7 @@ app.use(cors());
 //MongoDB Atlas Connection String
 mongoose.connect(process.env.MONGODB_URI);
 
-// app.use(routes);
+app.use(routes);
 
 //when in production connect the back end to the static build files
 if(process.env.NODE_ENV === 'production') {
@@ -33,60 +33,4 @@ app.listen(port, function() {
     console.log("express is running")
 })
 
-
-// mongoose 
-const postSchema = {
-    title: String,
-    author: String,
-    body: String,
-    date: Date
-};
-
-const Post = mongoose.model("Post", postSchema);
-
-// api routes
-
-//     grabbing all posts from our mongoDB and 
-//     sending that data to the front end in json format
-//     whenever the route is called
-    
-app.get('/posts', function(req, res) {
-    Post.find().then(posts => res.json(posts));
-});
-
-
-// add post
-
-app.post('/newPost', function(req, res) {
-   //deconstructing the object sent fron the front end
-   const title = req.body.title;
-   const author = req.body.author;
-   const body = req.body.body;
-   const date = req.body.date;
-   //creating new movie in DB using our model Movie
-   const newPost = new Post({
-       title,
-       author,
-       body,
-       date
-   })
-    //saving our new post
-    newPost.save()
-})
-
-// remove post
-app.delete('/delete/:id', function(req, res) {
-
-    // reconstruct id
-    const id = req.params.id;
-
-    //when the id of the model matches this id, delete
-    Post.findByIdAndDelete({_id: id}, function(err) {
-        if(!err) {
-            console.log("Post Removed Sucessfully");
-        } else console.log("Error post cannot be removed")
-    })
-
-
-})
 
