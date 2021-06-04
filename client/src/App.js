6b3614baf60b96da389d6e6de1b5250f92c24f7b
Catalina Williams <webdevcatalina@gmail.com>
2021-06-04 14:20:36 -0600

    added 'page not found' page that populates when a user tries to acess an incorrect URL within the site

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AllPosts from "./pages/AllPosts";
 import CreatePost from "./pages/CreatePost";
 import NavBar from "./components/NavBar"
-
+import NoMatch from "./pages/NoMatch"
 function App() {
   return (
     <Router>
@@ -12,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={AllPosts} />
           <Route exact path="/createPost" component={CreatePost} />
+          <Route component={NoMatch} />
         </Switch>
       </div>
     </Router>
