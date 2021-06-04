3e40109346075a558e45cb8aae534a276805f4ad
Catalina Williams <webdevcatalina@gmail.com>
2021-06-03 20:03:55 -0600

    added box to create post page

--- client/src/components/CreatePostForm/index.js
+++ client/src/components/CreatePostForm/index.js
@@ -1,43 +1,56 @@
 import React, { Component } from "react";
-import { Grommet, TextInput, Button, TextArea } from "grommet"
+import { Grommet, Box, CardHeader, CardBody, CardFooter,TextInput, Button, TextArea } from "grommet"
 
 function CreatePostForm(props) {
    console.log(props)
         return (
         <Grommet>
-             <form>
+       <Box
+  direction="row"
+  fill={true}
+  border={{ color: 'brand', size: 'large' }}
+  pad="medium"
+>
+<form>
 
-       <TextInput
-          placeholder="title"
-          value={props.title}
-          onChange={props.handleChange}
-          name="title"
-          />
+<TextInput
+   placeholder="title"
+   value={props.title}
+   onChange={props.handleChange}
+   name="title"
+   />
 
-           <TextInput 
-          placeholder="author"
-          value={props.author}
-          onChange={props.handleChange}
-          name="author"
-          />
+    <TextInput 
+   placeholder="author"
+   value={props.author}
+   onChange={props.handleChange}
+   name="author"
+   />
 
-          <TextArea 
-          placeholder="body"
-          value={props.body}
-         onChange={props.handleChange}
-          name="body"
-          />
+   <TextArea 
+   placeholder="body"
+   value={props.body}
+  onChange={props.handleChange}
+   name="body"
+   />
 
-          <TextInput
-          format="mm/dd/yyyy"
-          value={props.date}
-          onChange={props.handleChange}
-          name="date"
-          />
+   <TextInput
+   format="mm/dd/yyyy"
+   value={props.date}
+   onChange={props.handleChange}
+   name="date"
+   />
 
-          <Button onClick={props.addPost} primary label="Save Post" />
+   <Button onClick={props.addPost} primary label="Save Post" />
 
-        </form>
+ </form>
+</Box>
+      
+ <Box pad="small" background="dark-3" />
+  <Box pad="medium" background="light-3" />
+
+     
+             
         </Grommet>
         )
     
