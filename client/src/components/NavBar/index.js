066fafa7c5c99610465ff0daa23ea8ce1aaf6f67
Catalina Williams <webdevcatalina@gmail.com>
2021-06-08 17:50:03 -0600

    styled navbar with Grommet Icons

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Grommet, Button, Header, Icons } from "grommet";
-import { Grommet as GrommetIcon, Menu as MenuIcon } from 'grommet-icons';
+import { Grommet, Button, Header, Nav, Icon, Anchor } from "grommet";
+import { FormDown, Book, Edit, Slack } from 'grommet-icons';
 function NavBar() {
     return (
         <Grommet>
-        <Header background="brand">
+            <Nav alignSelf="center" direction="row" background="brand" pad="medium">
+    <Anchor  href={"/createPost"} icon={<Edit size="large" />} hoverIndicator />
+   
+    <Anchor href={"/"} icon={<Book size="large" />} hoverIndicator />
+</Nav>
+        {/* <Header background="brand">
   <Button href={"/"} label={"View Posts"} hoverIndicator />
-  <Button href={"createPost"} label={"Create Post"} hoverIndicator />
+  <Button href={"/createPost"} label={"Create Post"} hoverIndicator />
   
-</Header>
+</Header> */}
 </Grommet>
     )
 }
--- client/src/components/ViewAll/index.js
+++ client/src/components/ViewAll/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grommet, Card, CardHeader, CardBody, CardFooter, Button } from "grommet"
 
 
